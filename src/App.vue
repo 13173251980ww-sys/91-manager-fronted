be78025f2f6912ac1cf5a64d3369f8b28957fdda
amadeus <1685736247@qq.com>
2025-12-23 22:24:47 +0800

    feat: 仓库初始化

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import axios from "axios";
+  import {onMounted, ref} from "vue";
+
+  const name=ref("")
+  const getName=async function (){
+    const res=await axios.get("/api/test");
+    name.value=res.data.name;
+  }
+  onMounted(()=>{
+      getName()
+    }
+  )
+</script>
+
+
+<template>
+  <p>
+    {{name}}
+  </p>
+</template>
+
+<style scoped></style>
